b16c1100ebee5ab9021fb3fe29139941ddc616ec
Donald <dao008@uark.edu>
2018-04-10 21:36:27 -0500

    naming changes

--- src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
@@ -3,6 +3,10 @@ package edu.uark.models.entities.fieldnames;
 import edu.uark.dataaccess.entities.BaseFieldNames;
 
 public class TransactionFieldNames extends BaseFieldNames {
-	public static final String LOOKUP_CODE = "lookupcode";
-	public static final String COUNT = "count";
+	public static final String ID = "id";
+	public static final String CASHIER_ID = "cashierid";
+	public static final String TOTAL_AMOUNT = "totalamount";
+	public static final String IS_REFUND = "isrefund";
+	public static final String REFERENCE_ID = "referenceid";
+	public static final String CREATED_ON = "createdon";
 }
