42392ce977d58e9d25eb969ae9111c513f63107f
Donald <dao008@uark.edu>
2018-04-16 16:55:52 -0500

    Fixing Errors

---
+++ src/main/java/edu/uark/controllers/TransactionEntryRestController.java
@@ -0,0 +1,25 @@
+package edu.uark.controllers;
+
+import java.util.UUID;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uark.commands.transactionEntry.TransactionEntryCreateCommand;
+import edu.uark.models.api.TransactionEntry;
+@RestController
+@RequestMapping(value = "/api/transactionentry")
+
+public class TransactionEntryRestController {
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public TransactionEntry createTransactionEntry(@RequestBody TransactionEntry transactionEntry ) {
+        return (new TransactionEntryCreateCommand()).
+                setApiTransactionEntry(transactionEntry).
+                execute();
+    }
+}
