f6feab3e742d89efbf0615fcc5163a089688fdcf
Donald <dao008@uark.edu>
2018-04-30 17:58:10 -0500

    -m

--- src/main/java/edu/uark/commands/transaction/TransactionsQuery.java
+++ src/main/java/edu/uark/commands/transaction/TransactionsQuery.java
@@ -6,6 +6,7 @@ import edu.uark.commands.ResultCommandInterface;
 import edu.uark.models.api.Transaction;
 import edu.uark.models.repositories.TransactionRepository;
 import edu.uark.models.repositories.interfaces.TransactionRepositoryInterface;
+
 public class TransactionsQuery implements ResultCommandInterface<List<Transaction>> {
     @Override
     public List <Transaction> execute() {
@@ -30,4 +31,4 @@ public class TransactionsQuery implements ResultCommandInterface<List<Transactio
         this.transactionRepository = new TransactionRepository();
     }
 }
-}
+
